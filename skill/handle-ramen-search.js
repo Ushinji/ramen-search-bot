902023164df921f6b448a523534360432f22cf17
Ushinji <siguma971@gmail.com>
2017-11-08 19:12:06 +0900

    add carousel-reply

--- skill/handle-ramen-search.js
+++ skill/handle-ramen-search.js
@@ -111,7 +111,7 @@ module.exports = class HandlePizzaOrder {
           "columns": columns
         }
       };
-      console.log(`reply-msg:${message}`);
+      console.log(`reply-msg:${JSON.stringify(message)}`);
       // 最終的な応答を返す
       return bot.reply(message).then(
         (response) => {
